7842b1c15c6ff4545c1488667bb36818960e0522
Roopam Poddar <roopampoddar@Roopams-MacBook-Air.local>
2017-03-20 23:02:28 +0530

    first commit of the slim framework with some additions

---
+++ src/routes.php
@@ -0,0 +1,20 @@
+<?php
+// Routes
+
+$app->get('/[{name}]', function ($request, $response, $args) {
+    // Sample log message
+    $this->logger->info("Slim-Skeleton '/' route");
+
+    // Render index view
+    return $this->renderer->render($response, 'index.phtml', $args);
+});
+
+
+$app->get('/tickets', function (Request $request, Response $response) {
+    $this->logger->addInfo("Ticket list");
+    $mapper = new TicketMapper($this->db);
+    $tickets = $mapper->getTickets();
+
+    $response->getBody()->write(var_export($tickets, true));
+    return $response;
+});
